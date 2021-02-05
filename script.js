eada14e4a2d9e899031ead56d9d386b8d7ccb33f
Chris Paldino <cpaldino@stevens.edu>
2021-02-05 16:16:53 -0500

    Display
    Added responsiveness; adjusted error message

--- script.js
+++ script.js
@@ -15,15 +15,16 @@ $(document).ready(function () {
 		else {
 			m1 = eval(`[${m1}]`);
 			m2 = eval(`[${m2}]`);
-			if (m1.length !== m2[0].length) {
+			if (m1[0].length !== m2.length) {
 				result = "The number of rows in matrix 1 must equal the number of columns in matrix 2";
 			}
 			else {
 				result = matrixMult(m1, m2);
+				console.log(result);
 				let stringRes = "";
 				for (let i = 0; i < result.length; i++) {
 					//stringRes+=`${JSON.stringify(result[i])}`;
-					stringRes += result[i].toString();
+					stringRes += `[${result[i].toString()}]`;
 					if (i != result.length - 1) {
 						stringRes += "<br>";
 					}
